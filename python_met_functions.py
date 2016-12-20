984c2d5e79cfbd8ec838d1624f9f8caf81d08de2
gbromley <gbromley@wisc.edu>
2016-12-20 11:35:33 -0600

    first version of met functions file

---
+++ python_met_functions.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def f_to_c(tf):
+    return(5.0/9.0*(tf-32)) 
+
+def sat_vap_pres(tc):
+    return(0.611*np.exp((17.27*tc/(237.3+tc)))
+
+def rel_hum(tc, tcd):
+    e = 6.11*10.0**(7.5*tdc/(237.7+tdc))
+    es = return(6.11*10.0**(7.5*tc/(237.7+tc)))
+    return(100.0*e/es)
+
