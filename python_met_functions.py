62f72bbf94d9d786c588ffb3407b85a6912d339a
gbromley <gbromley@wisc.edu>
2016-12-20 13:47:31 -0600

    fixed import statements

--- python_met_functions.py
+++ python_met_functions.py
@@ -1,13 +1,10 @@
-
-# coding: utf-8
-
-# In[ ]:
+import numpy
 
 def f_to_c(tf):
     return(5.0/9.0*(tf-32)) 
 
 def sat_vap_pres(tc):
-    return(0.611*np.exp((17.27*tc/(237.3+tc)))
+    return(0.611*numpy.exp((17.27*tc/(237.3+tc))))
 
 def rel_hum(tc, tdc):
     e = 6.11*10.0**(7.5*tdc/(237.7+tdc))
