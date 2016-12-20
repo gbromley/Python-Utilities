f2c6e62ab77f10129360492f93ac2e5d1f5a5867
gbromley <gbromley@wisc.edu>
2016-12-20 13:36:18 -0600

    fixed variable definitions

--- python_met_functions.py
+++ python_met_functions.py
@@ -9,7 +9,7 @@ def f_to_c(tf):
 def sat_vap_pres(tc):
     return(0.611*np.exp((17.27*tc/(237.3+tc)))
 
-def rel_hum(tc, tcd):
+def rel_hum(tc, tdc):
     e = 6.11*10.0**(7.5*tdc/(237.7+tdc))
     es = (6.11*10.0**(7.5*tc/(237.7+tc)))
     return(100.0*e/es)
