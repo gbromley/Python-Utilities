e4e7b2a0026f295154077069b167d4c432d7bc52
gbromley <gbromley@wisc.edu>
2017-01-12 11:56:23 -0700

    added comments to make functions more clear

--- python_met_functions.py
+++ python_met_functions.py
@@ -2,14 +2,16 @@ import numpy
 
 def f_to_c(tf):
     return(5.0/9.0*(tf-32)) 
-
+#tc: temp in celsius
 def sat_vap_pres(tc):
     return(0.611*numpy.exp((17.27*tc/(237.3+tc))))
 
+# tdc: dewpoint temp in celsius tc: temp in celsius
 def rel_hum(tc, tdc):
     e = 6.11*10.0**(7.5*tdc/(237.7+tdc))
     es = (6.11*10.0**(7.5*tc/(237.7+tc)))
     return(100.0*e/es)
 
+# ea: vapor pressure es: sat. vapor pressure
 def vpd(ea,es):
     return (ea - es)
