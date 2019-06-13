2168700f527b9c3c2e8bbc6a5f9bcd1c36cc7ec4
gbromley <gabriel.bromley@montana.edu>
2019-06-13 10:04:49 -0600

    Added some code for plotting lat lon labels in LCC projected maps.

--- plotting_functions.py
+++ plotting_functions.py
@@ -1,112 +1,76 @@
-def plot_ngp_4seasons(lat, lon, var,pvals,levs,extent,colormp='RdBu_r',path_to_shape='/Users/gbromley/Dropbox/Montana_Climate_Project/Study_Area/Study_Area_08_01_17.shp', out_path='/Users/gbromley/Dropbox/Montana_Climate_Project/Model_paper/Figures/figure_new.png'){
+'''
+Script that helps with plotting lat/lon on lambert conformal
+Not my own work, found on github
+''''''
 
-#np.linspace(cb_min, cb_max, len(cus_col))
-    #create figure
+from copy import copy
 
-    #np.linspace(cb_min, cb_max, len(cus_col))
-    fig = plt.figure(1,figsize=(13,7), dpi=400.0)
-    #create projection
-    projection = ccrs.LambertConformal(central_longitude=-105,central_latitude=45,standard_parallels=[50,40])
-    ax1 = plt.subplot(2,2,1, projection = projection)
+%matplotlib inline
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
+import matplotlib.pyplot as plt
+import numpy as np
+import shapely.geometry as sgeom
 
-    cb = plt.contourf(lon,lat,var,transform=ccrs.PlateCarree(),levels=levs,cmap=colormp, extend='both')
-    pvals[0,:,:].plot.contourf(axes=ax1,transform=ccrs.PlateCarree(),color='none',edgecolor='black',hatches=["..."],alpha=0.,add_colorbar = False)
-    ax1.set_extent(extent)
-    states_provinces = cfeature.NaturalEarthFeature(
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='50m',
-        facecolor='none')
-    ax1.add_feature(states_provinces, edgecolor='lightgray')
-    ax1.add_feature(cfeature.COASTLINE)
-    ax1.add_feature(cfeature.BORDERS)
-    shape_feature = ShapelyFeature(Reader(path_to_shape).geometries(),crs=ccrs.PlateCarree(), facecolor='none',edgecolor='black')
-    ax1.add_feature(shape_feature)
-    ax1.title.set_text('DJF')
-    at = AnchoredText("a",
-                      prop=dict(size=8), frameon=True,
-                      loc=2,
-                      #backgroundcolor = 'lightgray'
-                      )
-    ax1.add_artist(at)
+def find_side(ls, side):
+    """
+    Given a shapely LineString which is assumed to be rectangular, return the
+    line corresponding to a given side of the rectangle.
 
-    ax2 = plt.subplot(2,2,2, projection = projection)
+    """
+    minx, miny, maxx, maxy = ls.bounds
+    points = {'left': [(minx, miny), (minx, maxy)],
+              'right': [(maxx, miny), (maxx, maxy)],
+              'bottom': [(minx, miny), (maxx, miny)],
+              'top': [(minx, maxy), (maxx, maxy)], }
+    return sgeom.LineString(points[side])
 
-    plt.contourf(spatial_trend.lon,spatial_trend.lat,spatial_trend[2,:,:].values,transform=ccrs.PlateCarree(),levels=levs,cmap=colormp, extend='both')
-    pvals[2,:,:].plot.contourf(axes=ax1,transform=ccrs.PlateCarree(),color='none',edgecolor='black',hatches=["..."],alpha=0.,add_colorbar = False)
 
-    ax2.set_extent(extent)
-    states_provinces = cfeature.NaturalEarthFeature(
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='50m',
-        facecolor='none')
-    ax2.add_feature(states_provinces, edgecolor='lightgray')
-    ax2.add_feature(cfeature.COASTLINE)
-    ax2.add_feature(cfeature.BORDERS)
-    ax2.add_feature(shape_feature)
-    bt = AnchoredText("b",
-                      prop=dict(size=8), frameon=True,
-                      loc=2,
-                      #backgroundcolor = 'lightgray'
-                      )
-    ax2.add_artist(bt)
-    ax2.set_title('MAM')
-    #ax2.title.set_visible(False)
+def lambert_xticks(ax, ticks):
+    """Draw ticks on the bottom x-axis of a Lambert Conformal projection."""
+    te = lambda xy: xy[0]
+    lc = lambda t, n, b: np.vstack((np.zeros(n) + t, np.linspace(b[2], b[3], n))).T
+    xticks, xticklabels = _lambert_ticks(ax, ticks, 'bottom', lc, te)
+    ax.xaxis.tick_bottom()
+    ax.set_xticks(xticks)
+    ax.set_xticklabels([ax.xaxis.get_major_formatter()(xtick) for xtick in xticklabels])
 
-    ax3 = plt.subplot(2,2,3, projection = projection)
 
-    plt.contourf(spatial_trend.lon,spatial_trend.lat,spatial_trend[1,:,:].values,transform=ccrs.PlateCarree(),levels=levs,cmap=colormp, extend='both')
-    #ax = plt.axes(projection=ccrs.LambertConformal())
-    pvals[1,:,:].plot.contourf(axes=ax1,transform=ccrs.PlateCarree(),color='none',edgecolor='black',hatches=["..."],alpha=0.,add_colorbar = False)
+def lambert_yticks(ax, ticks):
+    """Draw ricks on the left y-axis of a Lamber Conformal projection."""
+    te = lambda xy: xy[1]
+    lc = lambda t, n, b: np.vstack((np.linspace(b[0], b[1], n), np.zeros(n) + t)).T
+    yticks, yticklabels = _lambert_ticks(ax, ticks, 'left', lc, te)
+    ax.yaxis.tick_left()
+    ax.set_yticks(yticks)
+    ax.set_yticklabels([ax.yaxis.get_major_formatter()(ytick) for ytick in yticklabels])
 
-    ax3.set_extent(extent)
-    states_provinces = cfeature.NaturalEarthFeature(
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='50m',
-        facecolor='none')
-    ax3.add_feature(states_provinces, edgecolor='lightgray')
-    ax3.add_feature(cfeature.COASTLINE)
-    ax3.add_feature(cfeature.BORDERS)
-    ax3.add_feature(shape_feature)
-    ct = AnchoredText("c",
-                      prop=dict(size=8), frameon=True,
-                      loc=2,
-                      #backgroundcolor = 'lightgray'
-                      )
-    ax3.add_artist(ct)
-    ax3.set_title('JJA')
 
-    ax4 = plt.subplot(2,2,4, projection = projection)
-
-    plt.contourf(spatial_trend.lon,spatial_trend.lat,spatial_trend[3,:,:].values,transform=ccrs.PlateCarree(),levels=levs,cmap=colormp, extend='both')
-    #ax = plt.axes(projection=ccrs.LambertConformal())
-    pvals[3,:,:].plot.contourf(axes=ax1,transform=ccrs.PlateCarree(),color='none',edgecolor='black',hatches=["..."],alpha=0.,add_colorbar = False)
-    ax4.set_extent(extent)
-    states_provinces = cfeature.NaturalEarthFeature(
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='50m',
-        facecolor='none')
-    ax4.add_feature(states_provinces, edgecolor='lightgray')
-    ax4.add_feature(cfeature.COASTLINE)
-    ax4.add_feature(cfeature.BORDERS)
-    ax4.add_feature(shape_feature)
-    dt = AnchoredText("d",
-                      prop=dict(size=8), frameon=True,
-                      loc=2,
-                      #backgroundcolor = 'lightgray'
-                      )
-    ax4.add_artist(dt)
-    ax4.set_title('SON')
-    fig.subplots_adjust(top=0.9,bottom=0.1,left=0.25,right=0.75,hspace=0.1,wspace=0.1)
-    fig.suptitle('Temperature Trends 1970-2015')
-    cax = fig.add_axes((0.76, 0.12, 0.03, 0.76))
-    col_bar = fig.colorbar(cb,cax=cax)
-    col_bar.set_label('$^\circ$C / Decade',fontsize=10)
-
-    #plt.tight_layout()
-    plt.savefig(out_path),bbox_inches='tight')
-    plt.show()
-}
+def _lambert_ticks(ax, ticks, tick_location, line_constructor, tick_extractor):
+    """Get the tick locations and labels for an axis of a Lambert Conformal projection."""
+    outline_patch = sgeom.LineString(ax.outline_patch.get_path().vertices.tolist())
+    axis = find_side(outline_patch, tick_location)
+    n_steps = 30
+    extent = ax.get_extent(ccrs.PlateCarree())
+    _ticks = []
+    for t in ticks:
+        xy = line_constructor(t, n_steps, extent)
+        proj_xyz = ax.projection.transform_points(ccrs.Geodetic(), xy[:, 0], xy[:, 1])
+        xyt = proj_xyz[..., :2]
+        ls = sgeom.LineString(xyt.tolist())
+        locs = axis.intersection(ls)
+        if not locs:
+            tick = [None]
+        else:
+            tick = tick_extractor(locs.xy)
+        _ticks.append(tick[0])
+    # Remove ticks that aren't visible:
+    ticklabels = copy(ticks)
+    while True:
+        try:
+            index = _ticks.index(None)
+        except ValueError:
+            break
+        _ticks.pop(index)
+        ticklabels.pop(index)
+    return _ticks, ticklabels
